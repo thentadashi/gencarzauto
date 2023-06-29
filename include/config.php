d3e590020c302dab5cf9946d742d0cdea466d3cd
Toshtosh3 <thentadashi@gmail.com>
2023-06-30 05:01:41 +0800

    db

--- include/config.php
+++ include/config.php
@@ -1,7 +1,7 @@
 <?php
 defined('server') ? null : define("server", "localhost");
-defined('user') ? null : define ("user", "root") ;
-defined('pass') ? null : define("pass","");
+defined('user') ? null : define ("user", "genc3181_root1") ;
+defined('pass') ? null : define("pass","root1");
 defined('database_name') ? null : define("database_name", "genc3181_1") ;
 
 $this_file = str_replace('\\', '/', __File__) ;

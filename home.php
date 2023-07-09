0201c779f6cbb96788ce72e7bfa43c0d8aa51c9a
Toshtosh3 <thentadashi@gmail.com>
2023-07-09 22:43:31 +0800

    edit

--- home.php
+++ home.php
@@ -1,81 +1,25 @@
 
-    <section id="slider"><!--slider-->
+<section id="slider"><!--slider-->
     <div class="container">
       <div class="row">
         <div class="col-sm-12">
           <div id="slider-carousel" class="carousel slide" data-ride="carousel">
             <div class="carousel-inner">
               <div class="item active">
-                <div class="col-sm-6">
-                  <h1 cls="text-success" style="color:#D9602B;"><b>Hatchback Cars</b><br>
-                  <h2>Features</h2>
-                                <p>
-                                The cargo extension is the most distinguished feature of a hatchback. It can be found at the back of the rear seat. 
-                                A rear seat’s back can be moved forward to extend the ride’s cargo space. Today’s cargo space features a foldable extension that’s ideally created for maximum storage. 
-                                </p>
-                  <h4>Services</h4>
-                                <a href="#" class="btn btn-primary" style="margin-bottom: 5px;">Oil / Oil filter change </a>
-                                <a href="#" class="btn btn-primary"style="margin-bottom: 5px;">Wiper blades replacement </a>
-                                <a href="#" class="btn btn-primary"style="margin-bottom: 5px;">Replace air filter</a>
-                                <a href="#" class="btn btn-primary"style="margin-bottom: 5px;">Scheduled maintenance</a>
-                </div>
-                <div class="col-sm-6" style="margin-top:100px;">
-                  <img src="images/home/hatch-1.png" class="girl img-responsive" alt="" />
+                <div class="col-sm-12" style="margin:0px;margin-left:-50px">
+                  <img src="images/home/P2.JPG" class="girl img-responsive" alt="" />
                 </div>
               </div>
               <div class="item">
-                <div class="col-sm-6">
-                    <h1 class="text-primary" style="color:#007bff;"><b>Sedan Cars</b></h1>
-                    <h2>Features</h2>
-                                <p>
-                                    Four doors, two rows of seats, and separate compartments for the passengers, engine, and cargo. Sedans are generally the most fuel-efficient automotive
-                                     class that can comfortably accommodate four to five adults, which makes them a popular choice for family cars
-                                </p>
-                  <h4>Services</h4>
-                                <a href="#" class="btn " style="margin-bottom: 5px;background-color:#007bff;color:#f2f2f2;">Oil / Oil filter change </a>
-                                <a href="#" class="btn "style="margin-bottom: 5px;background-color:#007bff;color:#f2f2f2;">Wiper blades replacement </a>
-                                <a href="#" class="btn "style="margin-bottom: 5px;background-color:#007bff;color:#f2f2f2;">Replace air filter</a>
-                                <a href="#" class="btn "style="margin-bottom: 5px;background-color:#007bff;color:#f2f2f2;">Scheduled maintenance</a>
-                </div>
-                <div class="col-sm-6">
-                  <img src="images/home/sedan1.png" class="girl img-responsive" style="margin-top:110px;padding-bottom:93px;" alt="" />
+              <div class="col-sm-12" style="margin:0px;margin-left:-50px">
+                  <img src="images/home/P1.JPG" class="girl img-responsive" alt="" />
                 </div>
               </div>
-              
               <div class="item">
-                <div class="col-sm-6">
-                    <h1 style="color:#111;"><strong>Pick-up Trucks </strong></h1>
-                    <h2>Features</h2>
-                                <p>
-                                a light-duty truck that has an enclosed cabin, and a back end made up of a cargo bed that is enclosed by three low walls with no roof (this cargo bed back end sometimes consists of a tailgate and removable covering)
-                              <h4>Services</h4>
-                                <a href="#" class="btn " style="margin-bottom: 5px;background-color:#333;color:#f2f2f2;">Oil / Oil filter change </a>
-                                <a href="#" class="btn "style="margin-bottom: 5px;background-color:#333;color:#f2f2f2;">Wiper blades replacement </a>
-                                <a href="#" class="btn "style="margin-bottom: 5px;background-color:#333;color:#f2f2f2;">Replace air filter</a>
-                                <a href="#" class="btn "style="margin-bottom: 5px;background-color:#333;color:#f2f2f2;">Scheduled maintenance</a>
-                </div>
-                <div class="col-sm-6">
-                  <img src="images/home/pickup.png" class="girl img-responsive" style="margin-top:80px;" alt="" />
+              <div class="col-sm-12" style="margin:0px;margin-left:-50px">
+                  <img src="images/home/P3.JPG" class="girl img-responsive" alt="" />
                 </div>
               </div>
-
-              <div class="item">
-                <div class="col-sm-6">
-                    <h1 style="color:#111;"><strong>SUV Cars</strong></h1>
-                    <h2>Features</h2>
-                                <p>
-                                a fairly loose term but one that generally refers to stylish, sleek looking vehicles that offer elegant city driving but also handle rugged terrain thanks to a typical 4x4 capability. SUVs can come in any size – small, midsize or large.
-                              <h4>Services</h4>
-                                <a href="#" class="btn " style="margin-bottom: 5px;background-color:#333;color:#f2f2f2;">Oil / Oil filter change </a>
-                                <a href="#" class="btn "style="margin-bottom: 5px;background-color:#333;color:#f2f2f2;">Wiper blades replacement </a>
-                                <a href="#" class="btn "style="margin-bottom: 5px;background-color:#333;color:#f2f2f2;">Replace air filter</a>
-                                <a href="#" class="btn "style="margin-bottom: 5px;background-color:#333;color:#f2f2f2;">Scheduled maintenance</a>
-                </div>
-                <div class="col-sm-6">
-                  <img src="images/home/suv.png" class="girl img-responsive" style="margin-top:55px;" alt="" />
-                </div>
-              </div>
-              
             </div>
             
             <a href="#slider-carousel" class="left control-carousel hidden-xs sel" data-slide="prev">
